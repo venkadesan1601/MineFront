ce112ae1afbdc564f99b7f32d8ac4a78e92f272e
venkadesan1601 <venkadesan1601@gmail.com>
2018-06-05 22:48:20 +0530

    initial frame created

--- src/com/hiddenbrains/minefront/Display.java
+++ src/com/hiddenbrains/minefront/Display.java
@@ -1,8 +1,20 @@
 package com.hiddenbrains.minefront;
 
-public class Display {
+import com.hiddenbrains.minefront.Utils.DisplayUtils;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Display extends Canvas{
 
     public static void main(String[] args) {
-	// write your code here
+        Display game = new Display();
+        JFrame frame = new JFrame(DisplayUtils.TITLE_MINE_FRONT);
+        frame.add(game);
+        frame.setSize(DisplayUtils.WINDOW_WIDTH_800,DisplayUtils.WINDOW_HEIGHT_600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
---
+++ src/com/hiddenbrains/minefront/Utils/DisplayUtils.java
@@ -0,0 +1,8 @@
+package com.hiddenbrains.minefront.Utils;
+
+public class DisplayUtils {
+    //800*600
+    public static final int WINDOW_WIDTH_800 = 800;
+    public static final int WINDOW_HEIGHT_600 = 600;
+    public static final String TITLE_MINE_FRONT = "Mine Front";
+}
